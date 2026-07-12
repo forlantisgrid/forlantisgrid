b325a22e0b14c44d1215283785fe617632fdf9fa
Christina Lu <christinalu@Christinas-MacBook-Air.local>
2026-07-12 18:01:12 -0400

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  },
+  base: 'https://github.com/forlantisgrid/forlantisgrid',
   server: {
     port: 3000,
     open: true
