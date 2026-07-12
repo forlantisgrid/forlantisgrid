dc441d32dc4fe6f229af148b45439f3f5daf4553
Christina Lu <christinalu@Christinas-MacBook-Air.local>
2026-07-12 18:03:55 -0400

    remove base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/forlantisgrid/forlantisgrid',
   server: {
     port: 3000,
     open: true
