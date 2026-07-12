c9876fd636e8b89e4f720bd0fc0a4d58989e3d82
Christina Lu <christinalu@Christinas-MacBook-Air.local>
2026-07-12 18:52:01 -0400

    consolidate assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/forlantisgrid/',
+  publicDir: 'src/assets/public',
   server: {
     port: 3000,
     open: true
