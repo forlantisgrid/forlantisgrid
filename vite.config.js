40c57de344a6cedc82edf952924aad3353d4503f
Christina Lu <christinalu@Christinas-MacBook-Air.local>
2026-07-12 20:11:20 -0400

    move to assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/forlantisgrid/',
+  publicDir: 'src/assets',
   server: {
     port: 3000,
     open: true
